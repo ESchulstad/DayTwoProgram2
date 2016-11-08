503797c0a7ce7b833e64f7f7e835ee197cdc14f1
ESchulstad <eschulstad@gmail.com>
2016-11-07 21:02:31 -0500

    initial commit

---
+++ DayTwoProgram2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwoProgram2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >= 16)
+            {
+                Console.WriteLine("You can drive.");
+
+                if (x >= 18)
+                {
+                    Console.WriteLine("You're an adult.");
+                }
+                else
+                {
+                    Console.WriteLine("You're not an adult yet.");
+                }
+            }
+        }
+    }
+}
